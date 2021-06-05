d496a5119595f02fbd5f5862b85a21754b007f39
Visakhprajan <visakhprajan@gmail.com>
2021-06-05 09:54:46 +0530

    Updates

---
+++ precache-manifest.ab6921a896c862371384fdfa84cc1ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5a44abfed72c64d6a91b5d931c65d8",
+    "url": "/03-starting-project/index.html"
+  },
+  {
+    "revision": "2b7ec50c961d0702ab7c",
+    "url": "/03-starting-project/static/css/main.a565f700.chunk.css"
+  },
+  {
+    "revision": "a8b9dea63737512022ad",
+    "url": "/03-starting-project/static/js/2.03b8e3cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/03-starting-project/static/js/2.03b8e3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7ec50c961d0702ab7c",
+    "url": "/03-starting-project/static/js/main.6c5d09d1.chunk.js"
+  },
+  {
+    "revision": "b8df8d57a9bc4c2ea11c",
+    "url": "/03-starting-project/static/js/runtime-main.530520a9.js"
+  }
+]);
